e2fb339d77974232becd0c00423b7250cfa2b53c
Mars <pandesalsoftware@gmail.com>
2025-09-08 19:52:57 -0400

    [Save/Load] Fully Polished
    Fixed Backface Culling and Cosmetics that hold more than one material so it only changes the first material only on load!

--- Scenes/Game/AlterCreation/AlterCreationUI.gd
+++ Scenes/Game/AlterCreation/AlterCreationUI.gd
@@ -2438,7 +2438,8 @@ func _on_load_pressed():
 					if newInstance is MeshInstance3D:
 						var material = StandardMaterial3D.new()
 						material.albedo_color = loadedColor
-						newInstance.material_override = material
+						material.cull_mode = BaseMaterial3D.CULL_DISABLED
+						newInstance.set_surface_override_material(0, material)
 				
 		
 		#Skin Handling 
